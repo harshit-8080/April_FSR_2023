92480c928505255f0ef5464aa02f34b3eb934ca1
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-05-22 21:41:44 +0530

    JS basic day -1 added

---
+++ Javascript/1-variable.js
@@ -0,0 +1,25 @@
+/**
+ *
+ *  using these three words we can create variables...
+ *  let, var ,const
+ *
+ *  naming convention for variables... ( Left )
+ *  1. Always give some meaningful name.
+ *  
+ *
+ */
+
+let name = "harshit";
+
+let age = 24;
+
+let salary = 100.4;
+
+console.log("------->");
+console.log("My Name is  = ", name);
+console.log("My age = ", age);
+console.log("My salary = ", salary);
+
+console.log("==========> ");
+
+console.log(name, age, salary);
---
+++ Javascript/3-datatypes.js
@@ -0,0 +1,54 @@
+//? There are  total 8 different datatypes in JS;
+
+/**
+ *
+ *  1. string --> "harshit", 'raj', `jghdklgadhgafh`, "h"
+ *
+ *  2. number --> 78937283, 728, 8298.78837
+ *
+ *  3. bigint --> 1687670n;
+ *
+ *  4. undefined --> not defined yet may be defined later.
+ *
+ *  5. null --> it represents empty value.
+ *
+ *  6. object ---> key-pair value
+ *
+ *  7. boolean ---> true or false
+ *
+ *  8. symbol --> 
+ *
+ *
+ *
+ */
+
+// let address = "b";
+// console.log(typeof address);
+
+// let num = 78937283;
+// console.log(typeof num);
+
+// let b = 10n;
+// console.log(typeof b);
+
+// let b;
+// console.log(typeof b);
+
+// let c = null;
+// console.log(c);
+
+// let obj = { name: "harshit", age: 24, address: "hyd" };
+
+// console.log(obj);
+// console.log(typeof obj);
+
+// let married = false;
+// console.log(typeof married);
+
+// let x = Symbol("harshit"); // this is how you will create Symbol
+// console.log(typeof x);
+
+let x1 = "harshit";
+let x2 = "harshit";
+
+//! There is interesting thing happening behind the creation of containers or variables .............
