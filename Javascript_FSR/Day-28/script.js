f4bb3e6d0c9eea084464f1f9dae1f3ed152451bd
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-06-29 11:33:55 +0530

    music player added

--- Javascript_FSR/Day-28/script.js
+++ Javascript_FSR/Day-28/script.js
@@ -1,3 +1,4 @@
+//! Background Images Array For Each Song
 const bgc = [
   "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
   "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bXVzaWN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
@@ -21,26 +22,34 @@ for (let i = 0; i < playlistList.length; i++) {
   });
 }
 
+//! Function to play Song ()
 function playSong(currentSong) {
   let songUrl = playlistList[currentSong].getAttribute("data-src");
   audioPlayer.src = songUrl;
   audioPlayer.play();
   setActiveSong(currentSong); //! --->
+  playPause();
 }
 
+/**
+ * function to play next song
+ * 1. Increase currentSong by 1
+ * 2. Call playSong(currentSong)
+ */
 nextButton.addEventListener("click", function () {
   currentSong = currentSong + 1;
   playSong(currentSong);
   document.body.style.backgroundImage = `url(${bgc[currentSong]})`;
 });
 
+//! Function to play Prev Song
 prevButton.addEventListener("click", function () {
   currentSong = currentSong - 1;
   playSong(currentSong);
   document.body.style.backgroundImage = `url(${bgc[currentSong]})`;
 });
 
-//! --->
+//! Add a Active Song Black DT
 function setActiveSong(currentSong) {
   for (let i = 0; i < playlistList.length; i++) {
     if (i == currentSong) {
@@ -50,3 +59,53 @@ function setActiveSong(currentSong) {
     }
   }
 }
+
+//! Function for handling Play-Pause Feature
+let playButton = document.getElementById("playButton");
+playButton.addEventListener("click", function () {
+  playPause();
+});
+function playPause() {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    playButton.innerText = "Pause";
+  } else {
+    audioPlayer.pause();
+    playButton.innerText = "Play";
+  }
+}
+
+////! Handling Popup for Fav Songs And Add to LocalStorage
+let fav = document.querySelectorAll(".fav");
+// console.log("line 71 ==> ", fav);
+fav.forEach(function (button) {
+  button.addEventListener("click", function () {
+    let song = document.getElementById(`s${button.id}`);
+    localStorage.setItem(song.id, song.innerText.trim());
+  });
+});
+
+//! Get Songs From LocalStorage and Add to popup-content div
+let popupBtn = document.getElementById("popup-btn");
+let popupOverlay = document.getElementById("popup-overlay");
+let popupContent = document.getElementById("popup-content");
+
+popupBtn.addEventListener("click", function () {
+  popupOverlay.style.display = "flex";
+  let keys = Object.keys(localStorage);
+  // console.log(keys);
+
+  keys.forEach((key) => {
+    let songName = localStorage.getItem(key);
+    console.log(songName);
+
+    let p = document.createElement("p");
+    p.innerText = songName;
+    popupContent.appendChild(p); //TODO: Students WIll FIX IT......
+  });
+});
+
+let popupClose = document.getElementById("popup-close");
+popupClose.addEventListener("click", function () {
+  popupOverlay.style.display = "none";
+});
