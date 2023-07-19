494411fd34a8754b62f5704474280bc99c08bdc0
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-07-19 21:22:08 +0530

    day-3 added

---
+++ React/3-Intro to React | JSX | Components/my-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>This is my heading</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+        distinctio.
+      </p>
+      <button>CLick me</button>
+    </div>
+  );
+}
+
+export default App;
