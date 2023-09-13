5595e5a2ff823c5b80e21a21d2115c7c9b8d06f5
harshit-8080 <harshitraj@gytworkz.com>
2023-09-13 19:47:06 +0530

    os module added

---
+++ NodeJs/5-os/1-arch.js
@@ -0,0 +1,5 @@
+const os = require("os");
+
+const arch = os.arch();
+
+console.log(arch);
---
+++ NodeJs/5-os/3-mem.js
@@ -0,0 +1,14 @@
+const os = require("os");
+
+console.log(os.totalmem());
+console.log(os.freemem());
+
+console.log(os.platform());
+
+console.log(os.release());
+
+console.log(os.hostname());
+
+console.log(os.networkInterfaces());
+
+console.log(os.type());
